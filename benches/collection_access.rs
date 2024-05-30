7f951c319b69bef9875ae7c4cb1304612d033f99
jondavis847 <jondavis847@gmail.com>
2024-05-30 12:25:12 -0400

    added some access benchmarks

--- benches/collection_access.rs
+++ benches/collection_access.rs
@@ -1,8 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::collections::HashMap;
+use indexmap::IndexMap;
+use rustc_hash::FxHashMap;
+
+const N: usize = 1000;
 
 fn bench_array(c: &mut Criterion) {
-    let array: [i32; 1000] = [0; 1000];
+    let array: [i32; 1000] = [0; N];
     c.bench_function("array access", |b| {
         b.iter(|| {
             let _ = black_box(array[500]);
@@ -11,7 +15,7 @@ fn bench_array(c: &mut Criterion) {
 }
 
 fn bench_vec(c: &mut Criterion) {
-    let vec: Vec<i32> = vec![0; 1000];
+    let vec: Vec<i32> = vec![0; N];
     c.bench_function("vec access", |b| {
         b.iter(|| {
             let _ = black_box(vec[500]);
@@ -21,7 +25,7 @@ fn bench_vec(c: &mut Criterion) {
 
 fn bench_hashmap(c: &mut Criterion) {
     let mut hashmap: HashMap<usize, i32> = HashMap::new();
-    for i in 0..1000 {
+    for i in 0..N {
         hashmap.insert(i, i as i32);
     }
     c.bench_function("hashmap access", |b| {
@@ -31,5 +35,36 @@ fn bench_hashmap(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_array, bench_vec, bench_hashmap);
+fn bench_fxhashmap(c: &mut Criterion) {
+    let mut map = FxHashMap::default();
+    for i in 0..N {
+        map.insert(i, i as i32);
+    }
+
+    c.bench_function("FxHashMap lookup", |b| {
+        b.iter(|| {
+            for i in 0..N {
+                black_box(map.get(&i));
+            }
+        })
+    });
+}
+
+fn bench_indexmap(c: &mut Criterion) {
+    let mut map = IndexMap::new();
+    for i in 0..N {
+        map.insert(i, i as i32);
+    }
+
+    c.bench_function("IndexMap lookup", |b| {
+        b.iter(|| {
+            for i in 0..N {
+                black_box(map.get(&i));
+            }
+        })
+    });
+}
+
+
+criterion_group!(benches, bench_array, bench_vec, bench_hashmap, bench_fxhashmap, bench_indexmap);
 criterion_main!(benches);
